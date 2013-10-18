ef41b2c44f002318c3ca6d75ef383705d2c8f249
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-10-18 16:48:31 -0400

    code cleanup in gafftools.py

--- src/gafftools.py
+++ src/gafftools.py
@@ -6,9 +6,6 @@ import simtk.openmm.app.element
 import simtk.unit as unit
 import itertools
 
-GAFF_NONBONDED_PATH = "/home/kyleb/src/kyleabeauchamp/gaff2xml/xml/gaff_nonbonded.dat"
-GAFF_ELEMENTS_PATH = "/home/kyleb/src/kyleabeauchamp/gaff2xml/xml/gaff_elements.csv"
-
 gaff_elements = {'br': 'Br',
  'c': 'C',
  'c1': 'C',
@@ -99,16 +96,6 @@ def mol2_to_dataframes(filename):
 
     return atoms_frame, bonds_frame
 
-def load_gaff_nonbonded():
-    nonbonded = pd.read_fwf(GAFF_NONBONDED_PATH, widths=(4,16, 8), names=["atype", "sigma", "epsilon"], index_col=0)
-    return nonbonded
-
-def lookup_vdw(atype):
-    return nonbonded.ix[atype]
-
-nonbonded = load_gaff_nonbonded()
-gaff_elements = pd.read_csv(GAFF_ELEMENTS_PATH, index_col=0).element
-
 class Mol2Parser(object):
     def __init__(self, filename):
         self.atoms, self.bonds = mol2_to_dataframes(filename)
@@ -137,55 +124,4 @@ class Mol2Parser(object):
 
         return top_mm, xyz_mm
 
-
-def generate_gaff_xml(atoms, bonds):
-    residue_name = atoms.resName[1]  # To Do: Add check for consistency
-    xml_text = cStringIO.StringIO()
-    xml_text.write("<ForceField>\n")
-
-    xml_text.write("<AtomTypes>\n")
-    for (i, name, x, y, z, atype, code, resname, charge) in atoms.itertuples(False):
-        sigma, epsilon = lookup_vdw(atype)
-        full_name = residue_name + "_" + name
-        element = gaff_elements[atype]
-        omm_element = simtk.openmm.app.element.get_by_symbol(element)
-        mass = omm_element.mass / unit.daltons
-        
-        line = """<Type name="%s" class="%s" element="%s" mass="%f"/>\n""" % (full_name, atype, element, mass)
-        xml_text.write(line)
-    xml_text.write("</AtomTypes>\n")
-
-    xml_text.write("<Residues>\n")
-    xml_text.write("""<Residue name="%s">\n""" % residue_name)
-    for (i, name, x, y, z, atype, code, resname, charge) in atoms.itertuples(False):
-        sigma, epsilon = lookup_vdw(atype)
-        full_name = residue_name + "_" + name
-        element = gaff_elements[atype]
-        omm_element = simtk.openmm.app.element.get_by_symbol(element)
-        mass = omm_element.mass / unit.daltons        
-        line = """   <Atom name="%s" type="%s"/>\n""" % (name, full_name)
-        xml_text.write(line)
-    
-    for (id0, id1, bond_type) in bonds.itertuples(False):
-        i = id0 - 1  # Subtract 1 for zero based indexing in OpenMM???  
-        j = id1 - 1  # Subtract 1 for zero based indexing in OpenMM???  
-        xml_text.write("""<Bond from="%d" to="%d"/>\n""" % (i, j))
-        
-    xml_text.write("</Residue>\n")
-    xml_text.write("</Residues>\n")
-
-
-    xml_text.write("""<NonbondedForce coulomb14scale="0.833333" lj14scale="0.5">\n""")
-    for (i, name, x, y, z, atype, code, resname, charge) in atoms.itertuples(False):
-        sigma, epsilon = lookup_vdw(atype)
-        full_name = residue_name + "_" + name    
-        line = """<Atom type="%s" charge="%f" sigma="%f" epsilon="%f"/>\n""" % (full_name, charge, sigma, epsilon)
-        xml_text.write(line)
-    xml_text.write("</NonbondedForce>\n")
-    
-    xml_text.write("</ForceField>\n")
-    xml_text.reset()
-    return xml_text
-
-_FRCMOD_HEADERS = ["MASS", "BOND", "ANGL", "DIHE", "IMPR", "NONB"]
 
