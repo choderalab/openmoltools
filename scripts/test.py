a9bbbe201b1ca535013b8380902da17fd75c1aa1
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-10-18 16:39:17 -0400

    moved in new gafftools

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-import gafftools
-
-pybel_mol2, pandas_mol2 = gafftools.load_antechamber_mol2("./sustiva.mol2")
-pybel_mol2.atoms[6].type
-pybel_mol2.write("pdb", "out.pdb")
-
-xml = gafftools.generate_gaff_xml(pandas_mol2, pybel_mol2)
-f = open("./out.xml", 'w')
-f.writelines(xml.readlines())
