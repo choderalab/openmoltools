145d880a7d9be9a9b046fdb8131914739f223f77
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-06-12 12:21:42 -0400

    No need for valgrind

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -qq -y g++ gfortran valgrind csh
+sudo apt-get install -qq -y g++ gfortran csh
 sudo apt-get install -qq -y g++-multilib gcc-multilib
 wget http://repo.continuum.io/miniconda/Miniconda-3.0.5-Linux-x86_64.sh
 bash Miniconda-3.0.5-Linux-x86_64.sh -b
