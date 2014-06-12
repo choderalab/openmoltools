af5a1c54187a2759a79e5d18d86feb2927f25fb3
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-06-12 13:45:05 -0400

    Delay OE imports in tests

--- tests/test_drugs.py
+++ tests/test_drugs.py
@@ -1,12 +1,9 @@
 from unittest import skipIf
-
-import openeye.oechem
-
 from gaff2xml import utils
 
-#@skipIf(os.environ.get("TRAVIS", None) == 'true', "Skip testing of entire drug database on Travis.")
-@skipIf(True, "Skipping drugs.")
+@skipIf(os.environ.get("TRAVIS", None) == 'true', "Skip testing of entire drug database on Travis.")
 def test_drugs():
+    import openeye.oechem
     database_filename = utils.get_data_filename("chemicals/drugs/Zdd.mol2.gz")
     ifs = openeye.oechem.oemolistream(database_filename)
     for molecule in ifs.GetOEGraphMols():
@@ -14,9 +11,9 @@ def test_drugs():
             molecule_name, tripos_mol2_filename = utils.molecule_to_mol2(molecule)
             yield lambda : utils.test_molecule(molecule_name, tripos_mol2_filename)  # Cute trick to iteratively run this test over entire database.
 
-
+@skipIf(os.environ.get("TRAVIS", None) == 'true', "Skip testing of entire drug database on Travis.")
 def test_drug():
-    
+    import openeye.oechem
     database_filename = utils.get_data_filename("chemicals/drugs/Zdd.mol2.gz")
     ifs = openeye.oechem.oemolistream(database_filename)
     for molecule in ifs.GetOEGraphMols():
