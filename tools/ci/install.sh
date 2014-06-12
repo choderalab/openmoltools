867db074e3a6991d255e02fba5fbb2d6a60c34b7
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-06-12 13:12:58 -0400

    More simplification of conda install requirements

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -8,6 +8,6 @@ export PATH=$HOME/miniconda/bin:$PATH
 
 conda update --yes conda
 conda config --add channels http://conda.binstar.org/omnia
-conda create --yes -n ${python} --file tools/ci/requirements-conda-${python}.txt
+conda create --yes -n ${python} python=${python} --file tools/ci/requirements-conda.txt
 source activate $python
 #$HOME/miniconda/envs/${python}/bin/pip install $PIP_ARGS -r tools/ci/requirements-${python}.txt
