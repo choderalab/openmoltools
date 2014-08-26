c750c56482e8ba34a1f82665e506bbd29130fb7f
Bas Rustenburg <bas.rustenburg@choderalab.org>
2014-08-26 18:04:52 -0400

    Try no-default flag during install.

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -10,5 +10,5 @@ echo ${python}
 conda update --yes conda
 conda config --add channels http://conda.binstar.org/omnia
 conda config --add channels https://conda.binstar.org/ric
-conda create --yes -n ${python} python=${python} --file tools/ci/requirements-conda.txt
+conda create --yes --no-default-packages -n ${python} python=${python} --file tools/ci/requirements-conda.txt 
 source activate $python
