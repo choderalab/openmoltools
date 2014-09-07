bfb34b3d20f08d9905f422fbaaad3db376cfdc6b
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-09-07 10:58:36 -0400

    Fixed paths in devtools scripts

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -10,5 +10,5 @@ export PATH=$HOME/miniconda/bin:$PATH
 conda update --yes conda
 conda config --add channels http://conda.binstar.org/omnia
 conda config --add channels https://conda.binstar.org/ric
-conda create --yes -n ${python} python=${python} --file tools/ci/requirements-conda.txt
+conda create --yes -n ${python} python=${python} --file devtools/ci/requirements-conda.txt
 source activate $python
