61fb11b1436bf9335cb2cfd7b8fb8c7653f23985
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-09-07 12:43:47 -0400

    Added more tests of freesolv

--- tests/test_freesolv.py
+++ tests/test_freesolv.py
@@ -24,12 +24,13 @@ def test_load_freesolv_gaffmol2_vs_sybylmol2_vs_obabelpdb():
 
         database = pickle.load(open("./v0.3/database.pickle"))
         for key in database:
-            gaff_filename = os.path.abspath("./v0.3/mol2files_gaff/%s.mol2" % key)            
-            
-            cmd = """sed -i "s/<0>/LIG/" %s""" % gaff_filename
-            os.system(cmd)  # Have to remove the <0> because it leads to invalid XML in the forcefield files.
-            
-            t_gaff = md.load(gaff_filename)
+            for directory in ["mol2files_gaff", "mol2files_sybyl"]:
+                gaff_filename = os.path.abspath("./v0.3/%s/%s.mol2" % (directory, key))
+                
+                cmd = """sed -i "s/<0>/LIG/" %s""" % gaff_filename
+                os.system(cmd)  # Have to remove the <0> because it leads to invalid XML in the forcefield files.
+                
+                t_gaff = md.load(gaff_filename)
 
-            with utils.enter_temp_directory():        
-                 yield utils.tag_description(lambda : utils.test_molecule("LIG", gaff_filename), "Testing freesolv %s" % key)
+                with utils.enter_temp_directory():        
+                     yield utils.tag_description(lambda : utils.test_molecule("LIG", gaff_filename), "Testing freesolv %s %s" % (directory, key))
