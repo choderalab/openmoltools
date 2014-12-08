9536d09b69bcaf48990de1aed4cee8ff64c3ec35
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-12-08 15:34:36 -0500

    Updates

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -10,25 +10,12 @@ PIP_ARGS="-U"
 
 export PATH=$HOME/miniconda/bin:$PATH
 
-conda update --yes conda
-conda config --add channels http://conda.binstar.org/omnia
-source activate $python
-conda install --yes conda-build
-
-
-
-
 sudo apt-get update
-sudo apt-get install -qq -y g++ gfortran csh
-sudo apt-get install -qq -y g++-multilib gcc-multilib openbabel
-wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh
-bash Miniconda-3.6.0-Linux-x86_64.sh -b
-PIP_ARGS="-U"
-
-export PATH=$HOME/miniconda/bin:$PATH
+sudo apt-get install -qq -y g++ gfortran csh g++-multilib gcc-multilib openbabel
 
 conda update --yes conda
 conda config --add channels http://conda.binstar.org/omnia
 conda config --add channels https://conda.binstar.org/ric
 conda create --yes -n ${python} python=${python} --file devtools/ci/requirements-conda.txt
 source activate $python
+conda install --yes conda-build
