1caf671dff25d449d3433cf122947c2acb26430f
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-02-06 19:45:16 -0500

    Added test of common molecules to illustrate failures for catechol, aspirin.

---
+++ gaff2xml/tests/test_common_molecules.py
@@ -0,0 +1,41 @@
+"""
+Test some common molecules using OpenEye tools.
+
+"""
+
+import gaff2xml.openeye
+from nose.plugins.attrib import attr
+from unittest import skipIf
+from gaff2xml import utils
+import os
+
+try:
+    oechem = utils.import_("openeye.oechem")
+    if not oechem.OEChemIsLicensed(): raise(ImportError("Need License for OEChem!"))
+    oequacpac = utils.import_("openeye.oequacpac")
+    if not oequacpac.OEQuacPacIsLicensed(): raise(ImportError("Need License for oequacpac!"))
+    oeiupac = utils.import_("openeye.oeiupac")
+    if not oeiupac.OEIUPACIsLicensed(): raise(ImportError("Need License for OEOmega!"))
+    oeomega = utils.import_("openeye.oeomega")
+    if not oeomega.OEOmegaIsLicensed(): raise(ImportError("Need License for OEOmega!"))
+    HAVE_OE = True
+except:
+    HAVE_OE = False
+
+molecules = [
+    'benzene',
+    'toluene',
+    'phenol',
+    'catechol',
+    'aspirin',
+    ]
+
+@skipIf(not HAVE_OE, "Cannot run test_common_molecules() module without OpenEye tools.")
+def test_common_molecules():
+    import openeye.oechem
+    for molecule_name in molecules:
+        molecule = gaff2xml.openeye.iupac_to_oemol(molecule_name)
+        molecule = gaff2xml.openeye.get_charges(molecule)
+        with utils.enter_temp_directory():
+            molecule_name, tripos_mol2_filename = utils.molecule_to_mol2(molecule)
+            yield utils.tag_description(lambda : utils.test_molecule(molecule_name, tripos_mol2_filename), "Testing molecule %s" % molecule_name)
