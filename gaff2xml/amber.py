ac4d044b81eb4e74ff17eaca265ad9b7d88d23bf
kyleabeauchamp <kyleabeauchamp@gmail.com>
2015-03-13 15:41:05 -0400

    Initial checkin of amber-based pipeline for mixtures.

---
+++ gaff2xml/amber.py
@@ -0,0 +1,101 @@
+import mdtraj as md
+import tempfile
+import logging
+from .utils import getoutput
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format="LOG: %(message)s")
+
+
+# http://ambermd.org/tutorials/advanced/tutorial15/Tutorial2.xhtml
+# Run tLEaP with input file:
+# $ tleap -f commands.in
+
+TLEAP_TEMPLATE = """
+source leaprc.gaff
+%(mol2_section)s
+box = loadPdb %(box_filename)s
+%(amberparams_section)s
+setbox box centers
+saveAmberParm box %(prmtop_filename)s %(inpcrd_filename)s
+quit
+"""
+
+#loadmol2_section will look something like this:
+#BMI = loadmol2 bmi.mol2
+#BF4 = loadmol2 bf4.mol2
+#ACN = loadmol2 acn.mol2
+
+#loadamberparams_section looks like this:
+#loadamberparams frcmod.bf4
+#loadamberparams frcmod.bmi
+#loadamberparams frcmod.acn
+
+
+def build_mixture_prmtop(mol2_filenames, frcmod_filenames, box_filename, prmtop_filename, inpcrd_filename):
+    """Create a prmtop and inpcrd for an arbitrary mixture of liquids.
+
+    Parameters
+    ----------
+    mol2_filenames : list(str)
+        Filenames of GAFF flavored mol2 files.  Each must contain exactly
+        ONE ligand.
+    frcmod_filenames : str
+        Filename of input (GAFF!) mol2 file
+    box_filename : str
+        Use this frcmod filename
+    prmtop_filename : str
+        output prmtop filename.  Should have suffix .prmtop
+    inpcrd_filename : str
+        output inpcrd filename.  Should have suffix .inpcrd
+
+    Returns
+    -------
+    tleap_commands : str
+        The string of commands piped to tleap for building the prmtop 
+        and inpcrd files.  This will *already* have been run, but the
+        output can be useful for debugging or archival purposes.
+        
+    Notes
+    -----
+    This can be easily broken if there are missing, duplicated, or
+    inconsistent ligand residue names in your box, mol2, and frcmod files.
+    You can use mdtraj to edit the residue names with something like
+    this: trj.top.residue(0).name = "L1"
+    """
+    
+    # Check for one residue name per mol2 file and uniqueness between all mol2 files
+    all_names = set()
+    for filename in mol2_filenames:
+        t = md.load(filename)
+        names = set([r.name for r in t.top.residues])
+        
+        if len(names) != 1:
+            raise(ValueError("Must have a SINGLE residue name in each mol2 file."))
+        
+        all_names = all_names.union(list(names))
+
+    if len(all_names) != len(mol2_filenames):
+        raise(ValueError("Must have UNIQUE residue names in each mol2 file."))
+    
+    all_names = list(all_names)
+    
+    mol2_section = "\n".join("%s = loadmol2 %s" % (all_names[k], filename) for k, filename in enumerate(mol2_filenames))
+    amberparams_section = "\n".join("loadamberparams %s" % (filename) for k, filename in enumerate(frcmod_filenames))
+
+    tleap_commands = TLEAP_TEMPLATE % dict(mol2_section=mol2_section, amberparams_section=amberparams_section, box_filename=box_filename, prmtop_filename=prmtop_filename, inpcrd_filename=inpcrd_filename)
+    print(tleap_commands)
+    
+    file_handle = tempfile.NamedTemporaryFile('w')  # FYI Py3K defaults to 'wb' mode, which won't work here.
+    file_handle.writelines(tleap_commands)
+    file_handle.flush()
+
+    cmd = "tleap -f %s " % file_handle.name
+    logger.debug(cmd)
+
+    output = getoutput(cmd)
+    logger.debug(output)
+
+    file_handle.close()
+
+    return tleap_commands
---
+++ gaff2xml/tests/test_amber.py
@@ -0,0 +1,66 @@
+import numpy as np
+import mdtraj as md
+from unittest import skipIf
+import logging
+from mdtraj.testing import eq
+from gaff2xml import utils, amber,packmol
+import simtk.openmm as mm
+from distutils.spawn import find_executable
+
+
+logging.basicConfig(level=logging.DEBUG, format="LOG: %(message)s")
+
+@skipIf(packmol.PACKMOL_PATH is None, "Skipping testing of packmol conversion because packmol not found.")
+def test_amber_box():
+    etoh_filename = utils.get_data_filename("chemicals/etoh/etoh.mol2")
+    trj_list = [md.load(etoh_filename)]
+    
+    with utils.enter_temp_directory():  # Prevents creating tons of GAFF files everywhere.
+        
+        box_filename = "./box.pdb"
+        box_trj = packmol.pack_box(trj_list, [50])
+        box_trj.save(box_filename)
+    
+        gaff_mol2_filename1, frcmod_filename1 = utils.run_antechamber("etoh", etoh_filename, charge_method=None)
+        
+        mol2_filenames = [gaff_mol2_filename1]
+        frcmod_filenames =  [frcmod_filename1]
+        
+        prmtop_filename = "./out.prmtop"
+        inpcrd_filename = "./out.inpcrd"
+
+        tleap_cmd = amber.build_mixture_prmtop(mol2_filenames, frcmod_filenames, box_filename, prmtop_filename, inpcrd_filename)
+        print(tleap_cmd)
+
+
+@skipIf(packmol.PACKMOL_PATH is None, "Skipping testing of packmol conversion because packmol not found.")
+def test_amber_binary_mixture():
+    sustiva_filename = utils.get_data_filename("chemicals/etoh/etoh.mol2")
+    etoh_filename = utils.get_data_filename("chemicals/etoh/etoh_renamed.mol2")
+
+    trj0, trj1 = md.load(sustiva_filename), md.load(etoh_filename)
+    
+    # Hack to assign unique residue names that are consistent with contents of mol2 files
+    trj0.top.residue(0).name = "LIG"
+    trj1.top.residue(0).name = "LI2"
+    
+    trj_list = [trj0, trj1]
+    
+    with utils.enter_temp_directory():  # Prevents creating tons of GAFF files everywhere.
+        
+        box_filename = "./box.pdb"
+        box_trj = packmol.pack_box(trj_list, [25, 50])
+        box_trj.save(box_filename)
+        
+        gaff_mol2_filename0, frcmod_filename0 = utils.run_antechamber("sustiva", sustiva_filename, charge_method=None)
+        gaff_mol2_filename1, frcmod_filename1 = utils.run_antechamber("etoh", etoh_filename, charge_method=None)
+        
+        mol2_filenames = [gaff_mol2_filename0, gaff_mol2_filename1]
+        frcmod_filenames = [frcmod_filename0, frcmod_filename1]
+        
+        
+        prmtop_filename = "./out.prmtop"
+        inpcrd_filename = "./out.inpcrd"
+
+        tleap_cmd = amber.build_mixture_prmtop(mol2_filenames, frcmod_filenames, box_filename, prmtop_filename, inpcrd_filename)
+        print(tleap_cmd)
