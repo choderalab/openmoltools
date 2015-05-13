3cfef1c88228aa97664edc621ab9b08704b5045c
davidlmobley <dmobley@gmail.com>
2015-05-13 08:58:56 -0700

    Adding doc string and initial few lines of function for merging

--- openmoltools/gromacs.py
+++ openmoltools/gromacs.py
@@ -62,3 +62,53 @@ def read_next_topo_section(textarray):
    #Return the extra portion also
    extra=textarray[0:startline]
    return section,extra
+
+
+def merge_topologies( input_topologies, output_topology, system_name, molecule_names = None, molecule_numbers = None ):
+    """Merge GROMACS topology files specified in a list of input topologies and write the result into a final topology file specified. Optionally specify a list of molecule names to be used in the [ moleculetype ] and [ molecules ] sections, overriding what is already present.
+
+    Parameters
+    ----------
+    input_topologies : list (str)
+        A list of input topology files to be merged
+    output_topology : str
+        Output topology file to be written/created
+    system_name : str
+        Name to be used in final [ system ] section
+    molecule_names : list (str), optional
+        Molecule names to use in [ moleculetype ] and [ system ] sections in resulting topology file
+    molecule_numbers : list (int), optional
+        Molecule numbers to use in [ system ] section, overriding what is present in the existing [ system ] sections.
+
+    Returns
+    -------
+    status : bool
+        True if successful    
+
+    Notes
+    -----
+    This simply takes the contents of provided topology files and consolidates them to make a single resulting topology file. Existing [ moleculetype ] definitions are preserved and molecules are kept separate. 
+    This is not necessarily a particularly robust function, as it is expected to be replaced by functionality within ParmEd so we have not paid a great deal of attention to ensuring all possible topology file formats are handled.
+    """
+
+    #PRELIMINARIES
+    N_tops = len( input_topologies ) 
+
+    #Check for obvious input problems - do we have the right number of everything, do all the input files exist
+    if molecule_names <> None:
+        assert len( molecule_names ) == N_tops, "Must provide same number of molecule names as topology files." 
+    if molecule_numbers <> None:
+        assert len( molecule_numbers ) == N_tops, "Must provide same number of molecule numbers as topology files."
+    for filenm in input_topologies:
+        assert( os.path.isfile( filenm )), "Error: Can't find input file %s provided to merge_topologies." % filenm
+
+
+    #Read in topology files
+    topology_text = []
+    for filenm in input_topologies:
+        file = open(filenm, 'r')
+        topology_text.append(file.readlines() )
+        file.close()
+
+    #
+     
