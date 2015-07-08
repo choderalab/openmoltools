6ae9d408f8a76455847d656a071cac4a26c408d8
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-07-08 13:56:53 -0400

    Fix for travis tests of oechem.

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -14,7 +14,5 @@ sudo apt-get update
 sudo apt-get install -qq -y g++ gfortran csh g++-multilib gcc-multilib openbabel
 
 conda update --yes conda
-conda config --add channels http://conda.binstar.org/omnia
-conda config --add channels https://conda.binstar.org/rdkit
 source activate $python
 conda install --yes conda-build jinja2
