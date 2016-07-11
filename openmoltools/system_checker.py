22c213da32319339d94a22a89bea3b473253a9ec
davidlmobley <dmobley@gmail.com>
2016-07-11 14:13:06 -0700

    Bugfix

--- openmoltools/system_checker.py
+++ openmoltools/system_checker.py
@@ -367,7 +367,7 @@ class SystemChecker(object):
                     continue  # If both epsilon parameters are zero, then sigma doesn't matter so skip the comparison.
                 if parameter_name =="sigma":
                     assert compare(val0, val1), "Error: NonBondedForce Exception, atom (%d, %d) has sigma values of %f and %f angstroms, respectively." % (i0, i1, parameter_name, val0, val1)
-                elif parameter_name="qq":
+                elif parameter_name=="qq":
                     assert compare(val0, val1), "Error: NonBondedForce Exception atom (%d, %d) has charge values of %f and %f elementary charge, respectively." % (i0, i1, val0, val1)
                 else:
                     assert compare(val0, val1), "Error: NonBondedForce Exception, atom (%d, %d) has epsilon values of %f and %f kJ/mol, respectively." % (i0, i1, val0, val1)
