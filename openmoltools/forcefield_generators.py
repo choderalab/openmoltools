e2b33b734955610ef9123e200ffd5d828218749a
John Chodera <john.chodera@choderalab.org>
2020-02-15 17:07:00 +0100

    Fix horrible, unspeakable bugs

--- openmoltools/forcefield_generators.py
+++ openmoltools/forcefield_generators.py
@@ -351,9 +351,9 @@ def generateResidueTemplate(molecule, residue_atoms=None, normalize=True, gaff_v
     # Generate ffxml file contents for parmchk-generated frcmod output.
     leaprc = StringIO('parm = loadamberparams %s' % frcmod_filename)
     params = parmed.amber.AmberParameterSet.from_leaprc(leaprc)
-    params = parmed.openmm.OpenMMParameterSet.from_parameterset(params)
+    params = parmed.openmm.OpenMMParameterSet.from_parameterset(params, remediate_residues=False)
     ffxml = StringIO()
-    params.write(ffxml)
+    params.write(ffxml, write_unused=True)
 
     return template, ffxml.getvalue()
 
@@ -458,9 +458,9 @@ def generateForceFieldFromMolecules(molecules, ignoreFailures=False, generateUni
     # Generate ffxml file contents for parmchk-generated frcmod output.
     leaprc = StringIO(leaprc)
     params = parmed.amber.AmberParameterSet.from_leaprc(leaprc)
-    params = parmed.openmm.OpenMMParameterSet.from_parameterset(params)
+    params = parmed.openmm.OpenMMParameterSet.from_parameterset(params, remediate_residues=False)
     ffxml = StringIO()
-    params.write(ffxml)
+    params.write(ffxml, write_unused=True)
 
     # TODO: Clean up temporary directory.
     os.chdir(olddir)
